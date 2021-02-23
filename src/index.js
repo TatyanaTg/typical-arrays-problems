eb28e14b150de92cf9c46a7310ab10a26506f017
TatyanaTg <77569591+TatyanaTg@users.noreply.github.com>
2021-02-23 22:42:39 +0200

    init: start task -  typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
+exports.min = function mini(array) {
 
-exports.min = function min (array) {
-  return 0;
-}
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let min = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let max = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+};
+
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let sum = 0;
+        let count = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+            count++;
+        }
+        return sum / count;
+    }
+};
